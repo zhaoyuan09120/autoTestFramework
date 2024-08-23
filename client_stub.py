a5eee919efa0401af1fe4933c20e3d9d5fe342ee
赵园 <7858176+zhaoyuan0912@user.noreply.gitee.com>
2024-08-23 21:46:01 +0800

    fix conflit

--- client_stub.py
+++ client_stub.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from httpStubFramework.httpServerOperate import StubOperate
 
 # init stub
@@ -11,18 +11,4 @@ fileAppConfig = {
 fileAppStub = StubOperate(fileAppConfig["http_port"], fileAppConfig["socket_client_port"],
                           fileAppConfig["socket_server_port"])
 
-=======
-from httpStubFramework.httpServerOperate import StubOperate
-
-# init stub
-fileAppConfig = {
-    "http_port": 8771,
-    "socket_client_port": 8772,
-    "socket_server_port": 8773,
-}
-
-fileAppStub = StubOperate(fileAppConfig["http_port"], fileAppConfig["socket_client_port"],
-                          fileAppConfig["socket_server_port"])
-
->>>>>>> 79ffeff93db58c559e3491c93f439cd73c418f76
 print("init stub")
